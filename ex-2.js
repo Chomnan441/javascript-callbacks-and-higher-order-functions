08190d50e36b253682e027a2147da48d27673abc
Chomnan <chomnan415@gmail.com>
2026-06-09 12:57:51 +0700

    fix: update salary and score calculation functions

--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,26 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+    return score > 70?true:false;
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let countScoremore70=0;
+  for (let score of array){
+    operation(score)?countScoremore70+=1:countScoremore70;
+  }
+  if (countScoremore70 >=5){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result=atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result=atLeastFive(studentScoresRoom2,checkGradeOver70);
+let scoreRoom3Result=atLeastFive(studentScoresRoom3,checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
