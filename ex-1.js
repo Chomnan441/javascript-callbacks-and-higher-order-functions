e6ec1371484987eebb9654a204f62108a4a1322f
nutthakitsrida-wq <nutthakitsrida@gmail.com>
2026-06-09 14:31:18 +0700

    finishex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,26 +1,37 @@
-// Exercise #1: For Each Function
+/// Exercise #1: For Each Function
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
-  // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
-  // Start coding here
+  let result = [];
+
+  for (let item of array) {
+    result.push(operation(item));
+  }
+
+  return result;
 }
 
 // Using `forEach` function here
+let newEmployeeSalaries = forEach(
+  employeeSalaries,
+  addSalary5000
+);
 
-let newEmployeeSalaries;
-
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries);
+// [25005, 45000, 37000, 19500, 349000]
 
 /*
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+=> addSalary5000
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+=> forEach
 
 ====================================
 */
--- ex-2.js
+++ ex-2.js
@@ -1,24 +1,41 @@
-//Exercise #2: At Least Five Function
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
-  // Start coding here
+  return score > 70;
 }
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let count = 0;
+
+  for (let score of array) {
+    if (operation(score)) {
+      count++;
+    }
+  }
+
+  return count >= 5;
 }
 
-// Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(
+  studentScoresRoom1,
+  checkGradeOver70
+);
 
-console.log(scoreRoom1Result); //true
-console.log(scoreRoom2Result); //false
-console.log(scoreRoom3Result); //false
+let scoreRoom2Result = atLeastFive(
+  studentScoresRoom2,
+  checkGradeOver70
+);
+
+let scoreRoom3Result = atLeastFive(
+  studentScoresRoom3,
+  checkGradeOver70
+);
+
+console.log(scoreRoom1Result); // true
+console.log(scoreRoom2Result); // false
+console.log(scoreRoom3Result); // false
 
 /*
 ====================================
